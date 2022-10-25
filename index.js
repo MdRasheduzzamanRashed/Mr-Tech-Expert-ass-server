f83d518b5223b3aeab3aa9f86d4745ab9370654f
Md Rasheduzzaman Rashed <raseduzzaman15-7495@diu.edu.bd>
2022-10-26 00:03:23 +0600

    topic add

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
-const port = process.env.PORT || 5000;
+const app = express();
 app.use(cors());
+const port = process.env.PORT || 5000;
 
-const categories = require("./data/categories.json");
-const courses = require("./Data/data.json");
+const topics = require("./data/categories.json");
+const course = require("./Data/data.json");
 
 app.get("/", (req, res) => {
   res.send("Mr Tech Expert API Running");
 });
 
-app.get("/courses-topics", (req, res) => {
-  res.send(categories);
+app.get("/course-topics", (req, res) => {
+  res.send(topics);
 });
 
-app.get("/courses", (req, res) => {
-  res.send(courses);
-});
-
-app.get("/courses/:id", (req, res) => {
-  const category_course = course.filter((n) => n.category_id === id);
-  res.send(category_course);
+app.get("/course", (req, res) => {
+  res.send(course);
 });
 
 app.get("/course/:id", (req, res) => {
   const id = req.params.id;
-  const selectedCourse = course.find((n) => n._id === id);
+  const selectedCourse = course.find((n) => n.topic_id === id);
   res.send(selectedCourse);
 });
 
